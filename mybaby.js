ad3a8fc960d08ba717d39a46fbcb5a29403a1786
Axel-Go <j.axelgo24@gmail.com>
2026-02-14 04:01:27 +0800

    Update mybaby.js

--- mybaby.js
+++ mybaby.js
@@ -4,8 +4,16 @@ const letter = document.getElementById("letter");
 const flower = document.querySelector(".flower");
 
 envelope.addEventListener("click", () => {
+    
+    envelope.classList.add("fade-out");
+    setTimeout(() => {
+        envelope.style.display = "none";
+    }, 600);
+
+
     letter.classList.remove("hidden");
     letter.classList.add("show");
+
 
     flower.classList.remove("hidden");
     flower.classList.add("show");
