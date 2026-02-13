d6252c448baa723c81d28c6b3e311b2774f0a9d7
Axel-Go <j.axelgo24@gmail.com>
2026-02-14 01:35:29 +0800

    valentine upload

---
+++ mybaby.js
@@ -0,0 +1,13 @@
+
+const envelope = document.getElementById("envelope");
+const letter = document.getElementById("letter");
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+
+    letter.classList.remove("hidden");
+
+    setTimeout(() => {
+        letter.classList.add("show");
+    }, 10);
+});
